aad42be80ba747e33e42e26958cbea157e8849ea
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2025-02-04 17:18:16 +0800

    Bug fix for Pakta ang Laragway

--- Assets/Scripts/QuestionGame.cs
+++ Assets/Scripts/QuestionGame.cs
@@ -10,7 +10,7 @@ public class QuestionGame : MonoBehaviour
     [SerializeField] private GameObject secondOptionButton;
     [SerializeField] private Text questionText;
     [SerializeField] private Text letterText;
-    [SerializeField] private Image feedbackImage; // Image for feedback
+    [SerializeField] private GameObject feedbackImage; // Image for feedback
     [SerializeField] private Sprite correctImage; // Image to show for correct answers
     [SerializeField] private Sprite wrongImage; // Image to show for wrong answers
     [SerializeField] private List<Questions> questions;
@@ -24,42 +24,50 @@ public class QuestionGame : MonoBehaviour
 
     private void PrepareUi()
     {
-        firstOptionButton.transform.GetComponent<Button>().onClick.AddListener(() => CheckAnswer(0));
-        secondOptionButton.transform.GetComponent<Button>().onClick.AddListener(() => CheckAnswer(1));
+
+        GameObject.Find("firstOption").GetComponent<Button>().onClick.AddListener(() => CheckAnswer(0));
+        GameObject.Find("secondOption").GetComponent<Button>().onClick.AddListener(() => CheckAnswer(1));
         DisplayQuestion();
     }
 
     private void DisplayQuestion()
     {
-        firstOptionButton.transform.GetComponent<Image>().sprite = questions[currentQuestionIndex].firstOptionImage;
-        secondOptionButton.transform.GetComponent<Image>().sprite = questions[currentQuestionIndex].secondOptionImage;
+
+        Debug.Log(GameObject.Find("firstOption").GetComponent<RawImage>());
+        
+        
+        
+        GameObject.Find("firstOption").GetComponent<RawImage>().texture = questions[currentQuestionIndex].firstOptionImage.texture;
+        GameObject.Find("secondOption").GetComponent<RawImage>().texture = questions[currentQuestionIndex].secondOptionImage.texture;
         letterText.text = questions[currentQuestionIndex].letterQuestion;
         questionText.text = "Diin nga laragway ang nagapakita sang letra " + questions[currentQuestionIndex].phonicQuestion + " ?";
-        feedbackImage.gameObject.SetActive(false);  // Hide feedback image when displaying a new question
+        GameObject.Find("feedbackImage").SetActive(false);  // Hide feedback image when displaying a new question
     }
 
     private void CheckAnswer(int selectedIndex)
     {
-        feedbackImage.gameObject.SetActive(true);  // Show the feedback image
+        feedbackImage.SetActive(true);  // Show the feedback image
 
         if (selectedIndex != questions[currentQuestionIndex].correctOptionIndex)
         {
-            feedbackImage.sprite = wrongImage;  // Show wrong image
+            GameObject.Find("feedbackImage").GetComponent<RawImage>().texture = wrongImage.texture;  // Show wrong image
             Invoke("RetakeQuestion", 1.5f);  // Add a delay to show feedback, then retake the current question
             return;
         }
 
-        feedbackImage.sprite = correctImage;  // Show correct image
+        GameObject.Find("feedbackImage").GetComponent<RawImage>().texture = correctImage.texture;
+
+        //feedbackImage.transform.GetComponent<Image>().sprite = correctImage;  // Show correct image
 
         currentQuestionIndex++;
         if (currentQuestionIndex >= questions.Count)
-{
-    SceneManager.LoadScene("pakta_ang_laragway_FinishedScene"); // Replace with your finished scene name
-    return;
-}
+        {
+            SceneManager.LoadScene("pakta_ang_laragway_FinishedScene"); // Replace with your finished scene name
+            return;
+        }
 
-        
-        
+
+
 
         // Proceed to the next question after showing feedback
         Invoke("DisplayQuestion", 1.5f);  // Delay for feedback display before the next question
@@ -67,7 +75,7 @@ public class QuestionGame : MonoBehaviour
 
     private void RetakeQuestion()
     {
-        feedbackImage.gameObject.SetActive(false);  // Hide the feedback image
+        GameObject.Find("feedbackImage").SetActive(false);  // Hide the feedback image
         DisplayQuestion();  // Re-display the same question without advancing
     }
 }
