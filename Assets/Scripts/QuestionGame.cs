a9963afc25236ad2d6c755bc99f0c553e80fb35c
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2025-02-16 08:23:19 +0800

    VFX and UI improvements

--- Assets/Scripts/QuestionGame.cs
+++ Assets/Scripts/QuestionGame.cs
@@ -8,9 +8,12 @@ public class QuestionGame : MonoBehaviour
 {
     [SerializeField] private GameObject firstOptionButton;
     [SerializeField] private GameObject secondOptionButton;
+    [SerializeField] private AudioSource correctSound;
+    [SerializeField] private AudioSource wrongSound;
     [SerializeField] private Text questionText;
     [SerializeField] private Text letterText;
     [SerializeField] private GameObject feedbackImage; // Image for feedback
+    [SerializeField] private GameObject confetti;
     [SerializeField] private Sprite correctImage; // Image to show for correct answers
     [SerializeField] private Sprite wrongImage; // Image to show for wrong answers
     [SerializeField] private List<Questions> questions;
@@ -41,18 +44,24 @@ public class QuestionGame : MonoBehaviour
         GameObject.Find("secondOption").GetComponent<RawImage>().texture = questions[currentQuestionIndex].secondOptionImage.texture;
         letterText.text = questions[currentQuestionIndex].letterQuestion;
         questionText.text = "Diin nga laragway ang nagapakita sang letra " + questions[currentQuestionIndex].phonicQuestion + " ?";
-        GameObject.Find("feedbackImage").SetActive(false);  // Hide feedback image when displaying a new question
+        feedbackImage.SetActive(false);  // Hide feedback image when displaying a new question
+        confetti.SetActive(false);
     }
 
     private void CheckAnswer(int selectedIndex)
     {
         feedbackImage.SetActive(true);  // Show the feedback image
+        
 
         if (selectedIndex != questions[currentQuestionIndex].correctOptionIndex)
         {
+            wrongSound.Play();  // Play wrong sound
             GameObject.Find("feedbackImage").GetComponent<RawImage>().texture = wrongImage.texture;  // Show wrong image
             Invoke("RetakeQuestion", 1.5f);  // Add a delay to show feedback, then retake the current question
             return;
+        }else{
+            correctSound.Play();  // Play correct sound
+            confetti.SetActive(true);
         }
 
         GameObject.Find("feedbackImage").GetComponent<RawImage>().texture = correctImage.texture;
@@ -76,6 +85,7 @@ public class QuestionGame : MonoBehaviour
     private void RetakeQuestion()
     {
         GameObject.Find("feedbackImage").SetActive(false);  // Hide the feedback image
+        confetti.SetActive(false);
         DisplayQuestion();  // Re-display the same question without advancing
     }
 }
